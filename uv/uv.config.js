ba94fb62fa138e11d55818af6b615651bb88169f
Toast <123399463+Toast0012@users.noreply.github.com>
2024-01-21 11:15:33 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://phantom.delusionz.xyz/bare/',
+    bare: 'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
