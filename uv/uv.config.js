4de797ffde83d7b05ba0121c791f773f813e89b1
Toast <123399463+Toast0012@users.noreply.github.com>
2024-01-21 11:13:29 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://shuttleproxy.com/bare/',
+    bare: 'https://phantom.delusionz.xyz/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
